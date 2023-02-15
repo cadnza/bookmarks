6a59b1e58fb99e49dc260b39810fe94cbf050d31
Jon Dayley <jonathandayley@gmail.com>
2023-02-15 11:40:39 -0800

    Format

--- bookmarks/Classes/DataSource.swift
+++ bookmarks/Classes/DataSource.swift
@@ -22,8 +22,14 @@ struct DataSource {
 		// Read in contents
 		self.data = try? Data(contentsOf: configURL)
 		if let dataU = data {
-			guard let contentsWorking = try? JSONDecoder().decode([Item].self, from: dataU) else {
-				fputs("Can't read bookmarks; please delete '\(configURL.path)' and try again.",stderr)
+			guard
+				let contentsWorking = try? JSONDecoder()
+					.decode([Item].self, from: dataU)
+			else {
+				fputs(
+					"Can't read bookmarks; please delete '\(configURL.path)' and try again.",
+					stderr
+				)
 				throw Errors.cantParseConfig
 			}
 			self.contents = contentsWorking
