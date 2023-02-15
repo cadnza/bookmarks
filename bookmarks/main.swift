9f08763acf1b2f4e0ffe67723196c8c30197bb9a
Jon Dayley <jonathandayley@gmail.com>
2023-02-15 08:38:32 -0800

    Get going on basic structure

--- bookmarks/main.swift
+++ bookmarks/main.swift
@@ -1,7 +1,22 @@
 import ArgumentParser
 import Foundation
 
-let kHomePath: String = ProcessInfo.processInfo.environment["HOME"] ?? "/"
-let kConfigName = ".bookmarks"
-let kConfig: URL = URL(fileURLWithPath: kHomePath)
-	.appendingPathComponent(kConfigName)
+struct Bookmarks: ParsableCommand {
+
+	static let config: URL = URL(
+		fileURLWithPath: ProcessInfo.processInfo.environment["HOME"] ?? "/"
+	)
+	.appendingPathComponent(".bookmarks")
+
+	static let configuration = CommandConfiguration(
+		abstract: "Sweet and simple web bookmarks manager.",
+		subcommands: [Add.self]
+	)
+
+	struct Add: ParsableCommand {
+		func run() {
+			print(2) // TODO
+		}
+	}
+
+}
