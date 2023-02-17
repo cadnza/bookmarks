bdf2dd7175252ae1f87992d108f69d96476cbcd9
Jon Dayley <jonathandayley@gmail.com>
2023-02-17 08:16:51 -0800

    Organize validating logic

--- bookmarks/Subcommands/Update.swift
+++ bookmarks/Subcommands/Update.swift
@@ -30,32 +30,15 @@ extension Bookmarks {
 					"Please specify at least one parameter to update."
 				)
 			}
-			// Title has positive length
+			// Title
 			if let titleU = title {
+				// Title has positive length
 				guard titleU.hasPositiveLength() else {
 					exitWithError(
 						"Please specify a title with positive length."
 					)
 				}
-			}
-			// URL is valid
-			if let urlU = url {
-				let urlErrorMessage = "Please specify a valid URL."
-				let urlParsed = URL(string: urlU)
-				guard urlParsed != nil else {
-					exitWithError(urlErrorMessage)
-				}
-			}
-			// Tag has positive length
-			if let tagU = tag {
-				guard
-					tagU.hasPositiveLength()
-				else {
-					exitWithError("Please specify a tag with positive length.")
-				}
-			}
-			// Title is unique
-			if let titleU = title {
+				// Title is unique
 				guard
 					!ds.contents.filter({ $0.id != id }).map({ $0.title })
 						.contains(titleU)
@@ -63,13 +46,29 @@ extension Bookmarks {
 					exitWithError("A bookmark by that title already exists.")
 				}
 			}
-			// URL is unique
+			// URL
 			if let urlU = url {
+				// URL is valid
+				let urlErrorMessage = "Please specify a valid URL."
+				let urlParsed = URL(string: urlU)
+				guard urlParsed != nil else {
+					exitWithError(urlErrorMessage)
+				}
+				// URL is unique
 				guard
 					!ds.contents.filter({ $0.id != id })
 						.map({ $0.url.absoluteString }).contains(urlU)
 				else {
 					exitWithError("A bookmark to that URL already exists.")
+				}
+			}
+			// Tag
+			if let tagU = tag {
+				// Tag has positive length
+				guard
+					tagU.hasPositiveLength()
+				else {
+					exitWithError("Please specify a tag with positive length.")
 				}
 			}
 		}
