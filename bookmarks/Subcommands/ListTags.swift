939236f6f39902f1b2d62e4ea5f5a61a777c22dc
Jon Dayley <jonathandayley@gmail.com>
2023-02-18 11:11:40 -0800

    Flesh out json on list-tags command

--- bookmarks/Subcommands/ListTags.swift
+++ bookmarks/Subcommands/ListTags.swift
@@ -14,6 +14,42 @@ extension Bookmarks {
 		private var json = false
 
 		func run() {
+			guard !ds.contents.isEmpty else {
+				exitWithError("No bookmarks to show.")
+			}
+			let uniqueTags =
+				ds.contents.map { $0.tag }
+				.reduce(into: []) { result, x in
+					if !result.contains(where: { e in
+						e as? String == x
+					}) {
+						result.append(x as Any)
+					}
+				} as! [String?]
+			if json {
+				let toSerialize: [[String: Any?]] =
+					uniqueTags
+					.map { t in
+						[
+							"tag": t,
+							"count": ds.contents
+								.filter { c in
+									c.tag == t
+								}
+								.count,
+						]
+					}
+				let serialized = try! JSONSerialization.data(
+					withJSONObject: toSerialize,
+					options: [
+						.prettyPrinted, .sortedKeys, .withoutEscapingSlashes,
+					]
+				)
+				let jsonString = String(data: serialized, encoding: .utf8)!
+				print(jsonString)
+			} else {
+				// TODO
+			}
 		}
 	}
 
